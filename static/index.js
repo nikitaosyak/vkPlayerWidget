eaeb587a63e5c0d8a8a6e82bddc99ba0f8dd52b4
Nikita Osyak <desperatebassplayer@gmail.com>
2018-03-02 12:46:36 +0300

    added shitty layout

--- static/index.js
+++ static/index.js
@@ -1,8 +1,7 @@
 
 window.onload = function() {
-    console.log('preved')
-    const songContainer = document.getElementById('songContainer')
-    songContainer.innerHTML = 'akakak'
+    const songNameHTML = document.getElementById('songName')
+    const artistNameHTML = document.getElementById('artistName')
 
     const getSong = function() {
         fetch('/get_song', {
@@ -11,7 +10,10 @@ window.onload = function() {
             if (responce.status !== 200) { console.warn('failed to get song'); return }
             responce.json().then(function(json) {
                 console.log(json)
-                songContainer.innerHTML = json.song
+                // songContainer.innerHTML = json.song
+                const args = json.song.split('–')
+                songNameHTML.innerHTML = args[1].trim()
+                artistName.innerHTML =  'by ' + args[0].trim()
                 setTimeout(getSong, 2000)
             })
         }).catch(console.error)
