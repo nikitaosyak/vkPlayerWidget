771f70d7828dd601724aa5297c7976441ea41e9e
Nikita Osyak <desperatebassplayer@gmail.com>
2018-03-02 16:00:21 +0300

    new better UI

--- static/index.js
+++ static/index.js
@@ -1,5 +1,29 @@
+const SPEED_TIME = 5000;
+
+function initScroller(index, scroller){
+    console.log('initialized scroller')
+    scroller = $(scroller);
+    
+    var left = 0;
+    var scroll = function(){
+        setTimeout(function(){
+            console.log(scroller[0].scrollWidth, scroller[0].clientWidth)
+            if (left === 0){
+                left = scroller[0].scrollWidth - scroller[0].clientWidth
+            } else {
+                left = 0;
+            }
+
+            scroller.animate({scrollLeft: left}, SPEED_TIME, 'linear', scroll);
+        }, 1000)
+    }
+
+    scroll();
+}
 
 window.onload = function() {
+    $('.scroller').each(initScroller);
+
     const songNameHTML = document.getElementById('songName')
     const artistNameHTML = document.getElementById('artistName')
 
@@ -13,8 +37,8 @@ window.onload = function() {
                 // songContainer.innerHTML = json.song
                 const args = json.song.split('–')
                 songNameHTML.innerHTML = args[1].trim()
-                artistName.innerHTML =  'by ' + args[0].trim()
-                setTimeout(getSong, 2000)
+                artistName.innerHTML =  args[0].trim()
+                setTimeout(getSong, 10000)
             })
         }).catch(console.error)
     }
